09eccac13dfe444f25649ed65d8a1ce3429eaef3
Tom Elliott <tomelliottnz@gmail.com>
2022-06-24 09:58:57 +1200

    use ws dir for connection

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Rserve } from "@tmelliott/react-rserve";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Rserve {...{ host: "ws://137.184.226.172:8081" }}>
+    <Rserve {...{ host: "ws://137.184.226.172/ws" }}>
       <App />
     </Rserve>
   </React.StrictMode>

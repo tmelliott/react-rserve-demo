fbe618a344599309a8a55a7dbe5978fe4d10440d
Tom Elliott <tomelliottnz@gmail.com>
2022-06-24 13:39:25 +1200

    use wss

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { Rserve } from "@tmelliott/react-rserve";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Rserve {...{ host: "ws://reactr.cloud/ws" }}>
+    <Rserve {...{ host: "wss://reactr.cloud/ws" }}>
       <App />
     </Rserve>
   </React.StrictMode>
